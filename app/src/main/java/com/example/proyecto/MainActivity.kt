0efae89f1db58e4742d3392481dc48ef5ca1c2c3
JuanMorenoDeLaCasa <usuariojuanito@gmail.com>
2023-10-24 10:51:34 +0200

    Le alpicamos algo de color a las acrivities y enlazamos el boton de llamar con la actvity LLamar

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnllamar: Button
+    private lateinit var intent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnllamar = findViewById(R.id.boton_llamar)
+        btnllamar.setOnClickListener { view ->
+            intent = Intent(this, LLamar::class.java)
+            startActivity(intent)
+        }
     }
 }
