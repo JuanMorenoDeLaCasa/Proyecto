73ccfda5bbc1e65b16c5827400307248ba82797c
juan <usuariojuanito@gmail.com>
2023-10-28 16:53:01 +0200

    Cambiamos el estilo de la activity Alarma y ponemos botones en vez de switches, le damos funcionalidad y creamos 4 alarmas distintas

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.proyecto
 
-import android.Manifest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.net.Uri
-import android.os.Build
-import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +12,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var intentllamar: Intent
     private lateinit var btnalarma: Button
     private lateinit var intentalarma: Intent
-    private lateinit var btnurl: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         btnllamar = findViewById(R.id.boton_llamar)
         btnllamar.setOnClickListener { view ->
@@ -31,20 +28,8 @@ class MainActivity : AppCompatActivity() {
             intentalarma = Intent(this, Alarma::class.java)
             startActivity(intentalarma)
         }
-        initEvent()
     }
-    companion object {
-        const val url = "https://www.aemet.es/es/eltiempo/prediccion/municipios/jaen-id23050"
-    }
-    private fun initEvent() {
-        btnurl = findViewById(R.id.boton_url)
-        btnurl.setOnClickListener {(url())
-        }
-    }
-    private fun url(){
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
-        }
-    }
+
+
+}
 
