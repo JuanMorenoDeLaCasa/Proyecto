6e4cdda25921ddb2de36b76dae8460caea2f1fee
juan <usuariojuanito@gmail.com>
2023-10-28 21:23:16 +0200

    Creamos la activity correo y le damos funcionalidad, para que el usuario introduzca cuerpo, destinatario y asunto. Por otra parte he puesto un boton para que cuando le des te lleve a una url.

--- app/src/main/java/com/example/proyecto/Correo.kt
+++ app/src/main/java/com/example/proyecto/Correo.kt
@@ -1,11 +1,41 @@
 package com.example.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
+private lateinit var btnatrascorreo: Button
+private lateinit var btnenviarcorreo: Button
 class Correo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correo)
+
+        btnatrascorreo = this.findViewById(R.id.boton_atras_correo)
+        btnatrascorreo.setOnClickListener { view ->
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val editTextDestinatario = findViewById<EditText>(R.id.editTextDestinatario)
+        val editTextAsunto = findViewById<EditText>(R.id.editTextAsunto)
+        val editTextCuerpo = findViewById<EditText>(R.id.editTextCuerpo)
+        val btnenviarcorreo = findViewById<Button>(R.id.boton_nuevocorreo)
+
+        btnenviarcorreo.setOnClickListener {
+            val destinatario = editTextDestinatario.text.toString()
+            val asunto = editTextAsunto.text.toString()
+            val cuerpo = editTextCuerpo.text.toString()
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/rfc822"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(destinatario))
+            intent.putExtra(Intent.EXTRA_SUBJECT, asunto)
+            intent.putExtra(Intent.EXTRA_TEXT, cuerpo)
+
+            startActivity(Intent.createChooser(intent, "Enviar Correo"))
+        }
     }
 }
--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.proyecto
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 
@@ -12,6 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var intentllamar: Intent
     private lateinit var btnalarma: Button
     private lateinit var intentalarma: Intent
+    private lateinit var btncorreo: Button
+    private lateinit var intentcorreo: Intent
+    private lateinit var btnurl: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +34,22 @@ class MainActivity : AppCompatActivity() {
             intentalarma = Intent(this, Alarma::class.java)
             startActivity(intentalarma)
         }
+        btncorreo = findViewById(R.id.boton_correo)
+        btncorreo.setOnClickListener { view ->
+            intentcorreo = Intent(this, Correo::class.java)
+            startActivity(intentcorreo)
+        }
+            btnurl = findViewById(R.id.boton_url)
+            btnurl.setOnClickListener(View.OnClickListener {
+                val url = "https://www.aemet.es/es/eltiempo/prediccion/municipios/jaen-id23050"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+            })
+        }
+
     }
 
 
-}
+
 
